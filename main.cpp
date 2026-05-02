baf8b483b55496cda2635f9c9bc5e1370027b22a
davydov-egor <e.davydov.dev@gmail.com>
2026-05-02 23:56:06 +0400

    Add TCP socket with error handling

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <cerrno>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
 
 int main() {
-    std::cout << "Hello world" << std::endl;
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(sock == -1){
+        std::cerr << "Socket error: " << strerror(errno) << std::endl;
+        return 1;
+    }
+
+    std::cout << "Socket created: " << sock << std::endl;
+
+
+    close(sock);
     return 0;
 }
